2451d9aeca0e24d022a7869de9c3a076efcfdcd6
abhijit-kumar7 <abhijitkumar5577@gmail.com>
2024-04-01 12:03:01 +0530

    Revert "this is update testing java class"
    This reverts commit d0f326a35696e565352507c4fc4c9cde5081449c.

--- Testing.java
+++ Testing.java
@@ -2,6 +2,5 @@ public class Testing {
     public static void main(String[] args) {
         System.out.println("executed");
           System.out.println("pulling out new changes");
-        System.out.println("changed");
     }
 }
